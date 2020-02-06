3baf4f2dac9855f059f1d53c6df97c7403cd8a7c
Theo Morra <td512@td512.io>
2020-02-06 17:19:54 +1300

    fixes invalid plates and VINs

--- plugins/carjam.rb
+++ plugins/carjam.rb
@@ -14,31 +14,39 @@ infotext = Array.new
   sleep 1
   open(url+plate).read
   sleep 1
-  doc = Nokogiri::HTML(open(url+plate))
-  infotext.clear
-  attributes.each_with_index do |attrib, index|
-    doc.css("[data-key=#{attrib}] + .value").each do |item|
-        infotext.push("#{str[index]} #{item.inner_html.strip.gsub('<span class="terminal">', '').gsub(/(<[^>]*>)|\n|\t/s) {" "}}")
+  if open(url+plate).read["Plate or VIN"]
+    event.channel.send_embed do |e|
+      e.title = "#{plate.upcase} is invalid"
+      e.description = "#{plate.upcase} is either invalid, or has never been registered.\nValid plates are up to 6 characters long, VINs are 17"
+      e.color = 'ff0000'
     end
-  end
-  image = doc.at_css(".img.img-responsive.img-roundedd")["src"]
-  if image.chars.take(2).join == "//"
-    uri = "https:#{image}"
-  elsif image.chars.take(2).join == "/i"
-    uri = "https://www.carjam.co.nz#{image}"
   else
-    uri = image
-  end
-  if doc.at_css(".if-reported_stolen_location").to_s["Reported 1970-Jan-01"]
-    infotext.push("Stolen: No")
-  else
-    infotext.push("Stolen: ***YES***")
-  end
-  event.channel.send_embed do |e|
-    e.title = "CarJam Information for #{plate.upcase}"
-    e.url = url+plate
-    e.image = Discordrb::Webhooks::EmbedImage.new(url: uri)
-    e.description = infotext.join("\n")
-    e.color = '0000ff'
+    doc = Nokogiri::HTML(open(url+plate))
+    infotext.clear
+    attributes.each_with_index do |attrib, index|
+      doc.css("[data-key=#{attrib}] + .value").each do |item|
+        infotext.push("#{str[index]} #{item.inner_html.strip.gsub('<span class="terminal">', '').gsub(/(<[^>]*>)|\n|\t/s) {" "}}")
+      end
+    end
+    image = doc.at_css(".img.img-responsive.img-roundedd")["src"]
+    if image.chars.take(2).join == "//"
+      uri = "https:#{image}"
+    elsif image.chars.take(2).join == "/i"
+      uri = "https://www.carjam.co.nz#{image}"
+    else
+      uri = image
+    end
+    if doc.at_css(".if-reported_stolen_location").to_s["Reported 1970-Jan-01"]
+      infotext.push("Stolen: No")
+    else
+      infotext.push("Stolen: ***YES***")
+    end
+    event.channel.send_embed do |e|
+      e.title = "CarJam Information for #{plate.upcase}"
+      e.url = url+plate
+      e.image = Discordrb::Webhooks::EmbedImage.new(url: uri)
+      e.description = infotext.join("\n")
+      e.color = '0000ff'
+    end
   end
 end
