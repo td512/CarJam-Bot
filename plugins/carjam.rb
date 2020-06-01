9b5cf0912cae541c6f71dd352a70fd4949ca1a57
Theo Morra <td512@td512.io>
2020-06-02 01:06:29 +1200

    Adds submodel

--- plugins/carjam.rb
+++ plugins/carjam.rb
@@ -1,8 +1,8 @@
 require 'nokogiri'
 require 'open-uri'
 url = "https://www.carjam.co.nz/car/?plate="
-attributes = %w(year_of_manufacture make model main_colour body_style engine_number cc_rating fuel_type)
-str = %w(Year: Make: Model: Colour: Body\ Style: Engine\ Serial: CC\ Rating: Fuel\ Type:)
+attributes = %w(year_of_manufacture make model submodel main_colour body_style engine_number cc_rating fuel_type)
+str = %w(Year: Make: Model: Submodel: Colour: Body\ Style: Engine\ Serial: CC\ Rating: Fuel\ Type:)
 infotext = Array.new
 
 @bot.command(:carjam, help_available: false, chain_usable: false) do |event, text|
